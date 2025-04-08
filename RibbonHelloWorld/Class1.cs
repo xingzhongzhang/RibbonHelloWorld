fa952aa466a5ed5ca451c62a5f12b7d34ea0e461
兴中 张 <739217825@qq.com>
2025-04-08 08:28:51 +0800

    添加项目文件。

---
+++ RibbonHelloWorld/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ExcelDna.Integration;
+using ExcelDna.Integration.CustomUI;
+
+
+namespace RibbonHelloWorld
+{
+    public class Class1 : ExcelRibbon
+    {
+        public void OnButtonPressed(IRibbonControl control)
+        {
+            MessageBox.Show("Hello, World from Excel-DNA!", "Excel-DNA Custom Ribbon");
+        }
+
+        public string GetButtonLabel(IRibbonControl control)
+        {
+            return "Hello World";
+        }
+    }
+    public static class Functions
+    {
+        [ExcelFunction(Description = "Hello World function")]
+        public static string HelloWorld()
+        {
+            return "Hello, World from Excel-DNA!";
+        }
+    }
+}
